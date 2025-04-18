02570e8da16f383d14544191b10a95bc19edd0f6
Jason Chang <changjason96@gmail.com>
2025-04-18 13:59:17 -0400

    created initial head.js

---
+++ head.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔️⛔️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const head =function(input)
+{
+if(input.length===0)
+{
+  return ("undefined")
+}
+else
+{
+  return(input[0]);
+}
+}
+
+
+// TEST CODE
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
