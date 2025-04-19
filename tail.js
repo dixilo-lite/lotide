1ee675304cb27327a4bdcb1016b87d2b68822499
Jason Chang <changjason96@gmail.com>
2025-04-18 20:39:52 -0400

    added additional test requirements to tail.js

--- tail.js
+++ tail.js
@@ -21,3 +21,7 @@ const tail = function(arr) {
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(tail(words).length, words.length - 1);
+assertEqual(tail(words)[0], "Lighthouse");
+assertEqual(tail(["test"]).length, 0);
+assertEqual(tail([]).length, 0);
