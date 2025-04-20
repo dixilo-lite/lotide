91dbe826ba243fca476815700ebd0b931661837f
Jason Chang <changjason96@gmail.com>
2025-04-20 13:30:32 -0400

    addedd code to compare the length of the two arrays

--- eqArrays.js
+++ eqArrays.js
@@ -8,20 +8,21 @@ const assertEqual = function(actual,expected) {
 
 const eqArrays = function (arr1, arr2)
 {
+  if(arr1.length===arr2.length)
+  {
   for (let i = 0; i< arr1.length; i++)
   {
     if(arr1[i]===arr2[i])
     {
-      console.log(arr1[i],arr2[i]);
   
     }
     else {
-      console.log("false");
       return (false);
     }
   }
-  console.log("True");
   return (true);
+}
+return (false);
 }
 
 
@@ -30,3 +31,5 @@ eqArrays([1, 2, 3], [1, 2, 3]); // => true
 eqArrays([1, 2, 3], [3, 2, 1]); // => false
 eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
