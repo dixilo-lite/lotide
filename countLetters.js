7df9286e7d55337edbf5fec9059dce6bf762c898
Jason Chang <changjason96@gmail.com>
2025-04-23 22:48:34 -0400

    countLetters.js file created, used JSON.Stringify to compare two objects

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔️⛔️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (letters)
+{
+  let lettersObject={};
+  for (const char of letters)
+  {
+    if(lettersObject[char])
+    {
+      lettersObject[char]+=1;
+    }
+    else{
+      lettersObject[char]=1;
+    }
+  }
+  return lettersObject;
+};
+
+assertEqual(JSON.stringify(countLetters('lhl')), JSON.stringify({ l: 2, h: 1 }));
