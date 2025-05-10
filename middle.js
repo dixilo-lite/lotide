6fc30b2303eb133fd9776004a193ad0ffc6a893c
Jasonn Chang <changjason96@gmail.com>
2025-05-09 22:48:26 -0400

    changed variable type to const

--- middle.js
+++ middle.js
@@ -1,7 +1,7 @@
 const middle = function(arr)
 {
-  let arrLength = arr.length;
-  let middleIndex = Math.floor(arrLength / 2);
+  const arrLength = arr.length;
+  const middleIndex = Math.floor(arrLength / 2);
   if (arrLength <= 2) {
       return [];
     } else if (arrLength % 2 === 0) {
