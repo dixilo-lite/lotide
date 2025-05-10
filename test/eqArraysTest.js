8d5e97bd1a7aa15148ac340edb7539fec26808c9
Jasonn Chang <changjason96@gmail.com>
2025-05-09 22:32:40 -0400

    initial commit

---
+++ test/eqArraysTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require ("../assertEqual");
+const eqArray = require ("../eqArrays");
+
+qArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false); // => false
