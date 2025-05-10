a8dc12ff8afdf034b97aefe6f2cea9d46ca2854f
Jasonn Chang <changjason96@gmail.com>
2025-05-09 23:46:06 -0400

    refactored with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,26 @@
-const eqArrays = require("../eqArrays");
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require ("chai").assert;
 const middle = require ("../middle");
+/*
+
 assertArraysEqual(middle([1]),[]); // => [])
 assertArraysEqual(middle([1, 2]),[]); // => []
 
 assertArraysEqual(middle([1, 2, 3]),[2]); // => [2]
 assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
+*/
+describe("#middle",()=>{
+  it("should return an empty array if there is an array with 1 element inside",()=>{
+    assert.strictEqual(middle([1]).length,0);
+  });
+
+   it("should return an empty array if there is an array with 2 element inside",()=>{
+    assert.strictEqual(middle([1,2]).length,0);
+  });
+   it("should return an array with one value if there is an array with 3 element inside",()=>{
+    assert.strictEqual(middle([1,2,3]).length,1);
+  });
+
+   it("should return an array with one value if there is an array with 3 element inside",()=>{
+    assert.strictEqual(middle([1,2,3])[0],2);
+  });
+});
