7979020e081ef3f19540cdc03af913135ddbecbb
Jasonn Chang <changjason96@gmail.com>
2025-05-09 21:26:27 -0400

    refactored and created test files

--- head.js
+++ head.js
@@ -1,23 +1,12 @@
-const assertEqual = function(actual,expected) {
-  if (actual === expected) {
-    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔️⛔️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-const head =function(input)
-{
-if(input.length===0)
-{
+const assertEqual = require ("./assertEqual");
+const head = function(input) {
+if (input.length === 0) {
   return undefined;
-}
-else
-{
+} else  {
   return(input[0]);
-}
+  }
 }
 
+module.exports =head;
 
 // TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
