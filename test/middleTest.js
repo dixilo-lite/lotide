ee5b91c4e48c0769d0736ae6b50f96994ef91ac1
Jasonn Chang <changjason96@gmail.com>
2025-05-09 22:46:58 -0400

    initial commit

---
+++ test/middleTest.js
@@ -0,0 +1,8 @@
+const eqArrays = require("../eqArrays");
+const assertArraysEqual = require("../assertArraysEqual");
+const middle = require ("../middle");
+assertArraysEqual(middle([1]),[]); // => [])
+assertArraysEqual(middle([1, 2]),[]); // => []
+
+assertArraysEqual(middle([1, 2, 3]),[2]); // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
