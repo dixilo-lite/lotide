98307d0861d6327dd4493159cf3c526f6afecd87
Jasonn Chang <changjason96@gmail.com>
2025-05-09 22:40:13 -0400

    refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,3 @@
-const eqArrays = function (arr1, arr2)
-{
-  if(arr1.length===arr2.length)
-  {
-  for (let i = 0; i< arr1.length; i++)
-  {
-    if(arr1[i]!==arr2[i])
-    {
-      return (false);
-    }
-  }
-  return (true);
-}
-return (false);
-}
-
 const assertArrayEqual = function(arr1,arr2)
 {
   if(eqArrays(arr1,arr2))
@@ -25,5 +9,6 @@ const assertArrayEqual = function(arr1,arr2)
   }
 }
 // TEST CODE
-assertArrayEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArrayEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+
+
+module.exports = assertArrayEqual;
